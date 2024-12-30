85f77b9e5e9efac4b83c22773b37e2414214c363
uitangclan <uitangclan@gmail.com>
2024-12-30 11:02:52 +0900

    happy coding

---
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
@@ -0,0 +1,62 @@
+package com.example.reply.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.example.reply.ui.ReplyApp
+import org.junit.Rule
+import org.junit.Test
+
+class ReplyAppTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    @TestCompactWidth
+    fun compactDevice_verifyUsingBottomNavigation() {
+        // Set up compact window
+        composeTestRule.setContent {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact
+            )
+        }
+        // Bottom navigation is displayed
+        composeTestRule.onNodeWithTagForStringId(
+            com.example.reply.R.string.navigation_bottom
+        ).assertExists()
+    }
+
+    @Test
+    @TestMediumWidth
+    fun mediumDevice_verifyUsingNavigationRail() {
+        // Set up compact window
+        composeTestRule.setContent {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Medium
+            )
+        }
+        // Bottom navigation is displayed
+        composeTestRule.onNodeWithTagForStringId(
+            com.example.reply.R.string.navigation_rail
+        ).assertExists()
+    }
+
+    @Test
+    @TestExpandedWidth
+    fun expandedDevice_verifyUsingNavigationDrawer() {
+        // Set up compact window
+        composeTestRule.setContent {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Expanded
+            )
+        }
+        // Bottom navigation is displayed
+        composeTestRule.onNodeWithTagForStringId(
+            com.example.reply.R.string.navigation_drawer
+        ).assertExists()
+    }
+
+
+
+}
